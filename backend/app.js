4994c301e82c33873ac329cba340c3946a949c0f
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-09-28 19:56:32 +0530

    add the basic setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,11 @@
-console.log("test");
+import express from "express";
+
+const app = express();
+
+app.get("/home", (req, res) => {
+    res.send("Home")
+})
+
+app.listen(8000, ()=> {
+    console.log("listening to the port 8000");
+})
