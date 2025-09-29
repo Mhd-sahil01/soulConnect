8e671f3f1db717c0b40a890f4e9dba3487772306
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-09-29 10:23:14 +0530

    connect mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
 import express from "express";
+import { createServer } from "node:http";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const server = createServer(app);
+
+app.set("port", (process.env.PORT || 8000));
 
 app.get("/home", (req, res) => {
     res.send("Home")
 })
 
-app.listen(8000, ()=> {
-    console.log("listening to the port 8000");
-})
+async function start() {
+    const connectDB = await mongoose.connect(process.env.MONGO_URL)
+    console.log(`connected to database ${connectDB.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("listening to the port 8000");
+    })
+}
+start();
