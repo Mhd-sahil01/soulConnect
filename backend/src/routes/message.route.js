bc92a3c01649244d79db308a820a8630f013bdcb
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-11-17 20:47:46 +0530

    rename router variable and update imports and  add react-router dependency

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,9 +2,9 @@ import express from "express";
 import { getMessages } from "../controllers/message.controller.js";
 import { protectedRoute } from "../middlewares/auth.middleware.js";
 
-const router = express.Router();
+const messageRouter = express.Router();
 
 // router.post("/send/:pairId", protectedRoute, sendMessage);
-router.get("/:pairId", protectedRoute, getMessages);
+messageRouter.get("/:pairId", protectedRoute, getMessages);
 
 export default messageRouter;
