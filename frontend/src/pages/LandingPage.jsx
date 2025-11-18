beb9e4efd6f16b8e57db5c5fe48b3bd80fadeec2
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-11-18 16:03:40 +0530

    add Particles component and integrate it into LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,17 @@
+import Particles from '../components/Particles.jsx';
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center mt-10 text-amber-300">Welcome to SoulConnect</h1>
-      <p className="text-center mt-4 text-lg text-gray-300">Connecting souls through technology.</p>
-    </div>
+    <div className="relative w-full h-screen bg-black">
+            <Particles
+                particleColors={['#027FAE', '#027FAE']}
+                particleCount={300}
+                particleSpread={10}
+                speed={0.1}
+                particleBaseSize={130}
+                moveParticlesOnHover={true}
+                alphaParticles={false}
+            />
+        </div>
   )
 }
