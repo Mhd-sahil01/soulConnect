d01b30cb37a02e05d01ff7a8358ff9d0f2deb0d4
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-11-24 12:42:02 +0530

    adjust LandingPage layout for improved responsiveness

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -7,15 +7,15 @@ export default function LandingPage() {
         <>
             <Navbar />
             <div className="relative h-screen w-full bg-black overflow-hidden">
-                <div className="w-screen h-screen md:h-[80vh] absolute flex flex-col md:flex-row items-center justify-center md:mt-20 px-10 gap-9 md:gap-5 z-20">
-                    <div className="flex flex-col justify-center item-center md:items-start ml-5 md:ml-10 md:max-w-xl">
-                        <h1 className="text-2xl md:text-4xl font-bold text-white mb-4">Feel Close, No Matter the Distance</h1>
+                <div className="absolute h-[90%] w-full md:w-[90%] flex flex-col md:flex-row justify-center items-center gap-4 px-6 top-0 bottom-0 my-auto md:left-0 md:right-0 md:mx-auto z-20">
+                    <div className="flex flex-col max-w-xl border p-5 z-20">
+                        <h1 className="hover-glow text-2xl md:text-4xl font-bold text-white mb-4">Feel Close, No Matter the Distance</h1>
                         <p className="text-md md:text-lg text-gray-300"> SoulConnect lets you call, chat, and watch videos together.</p>
-                        <p className="text-md md:text-lg text-gray-300 mb-4"> Making long-distance feel effortless</p>
-                        <button className="px-6 py-3 bg-[#027FAE] hover:bg-[#0393C9] text-white font-semibold rounded-full  transition-all duration-300 hover:scale-105"> Connect Now </button>
+                        <p className="text-sm md:text-lg text-gray-300 mb-4"> Making long-distance feel effortless</p>
+                        <button className="px-6 py-3 md:w-max bg-[#027FAE] hover:bg-[#0393C9] text-white font-semibold rounded-full transition-all duration-300 hover:scale-105"> Connect Now </button>
                     </div>
 
-                    <div className="flex justify-center items-center">
+                    <div className="flex justify-center w-fit items-center">
                         <Carousel
                             baseWidth={300}
                             autoplay={true}
