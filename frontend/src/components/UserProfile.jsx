ff73956ffe8f90a523462c3fe6f2322b1c7ab5c9
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-12-05 22:26:41 +0530

    Add UserProfile component and integrate it into Dashboard for user display

---
+++ frontend/src/components/UserProfile.jsx
@@ -0,0 +1,62 @@
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+export default function UserProfile() {
+
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        '& .MuiBadge-badge': {
+            backgroundColor: '#22C55E',
+            color: '#22C55E',
+            boxShadow: `0 0 0 2px #0C0F13`,
+            '&::after': {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                borderRadius: '50%',
+                animation: 'ripple 1.6s infinite ease-in-out',
+                border: '1px solid currentColor',
+                content: '""',
+            },
+        },
+        '@keyframes ripple': {
+            '0%': {
+                transform: 'scale(.8)',
+                opacity: 0.9,
+            },
+            '100%': {
+                transform: 'scale(2.2)',
+                opacity: 0,
+            },
+        },
+    }));
+
+    return (
+        <Stack direction="row" spacing={1} className="items-center">
+            <StyledBadge
+                overlap="circular"
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                variant="dot"
+            >
+                <Avatar 
+                    alt="User"
+                    src=""
+                    sx={{
+                        bgcolor: "#1A1D21",        // dark avatar background
+                        width: 40,
+                        height: 40,
+                        border: "2px solid #0C0F13"
+                    }}
+                />
+            </StyledBadge>
+
+            <div className="flex flex-col justify-center">
+                <div className="text-xs md:text-sm text-green-500">online</div>
+                <div className="text-xs md:text-sm text-gray-300">Name of the per</div>
+            </div>
+        </Stack>
+    );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { MessageSquare, Phone, Video, Gamepad2, Tv } from "lucide-react";
 import { useNavigate } from "react-router";
+import UserProfile from "../components/UserProfile";
 
 export default function CollaborativeDashboard() {
     const navigate = useNavigate();
@@ -13,14 +14,14 @@ export default function CollaborativeDashboard() {
     ];
 
     return (
-        <div className=" h-screen w-full bg-black overflow-hidden text-white border border-red-700">
-            <div className="flex justify-between px-4">
-                <div>user1</div>
-                <div>user2</div>
+        <div className=" h-screen w-full bg-black overflow-hidden text-white">
+            <div className="flex justify-between p-4">
+                <UserProfile/>
+                <UserProfile/>
             </div>
-            <div className="h-[90%] w-full md:w-[90%] flex flex-col justify-center items-center gap-6 px-6 md:mx-auto border border-amber-300">
-                <h1 className="text-2xl md:text-3xl font-bold">Collaboration Hub</h1>
-                <div className=" flex flex-wrap justify-center gap-5 w-full md:max-w-lg border border-[#1B444B]/70 p-4">
+            <div className="h-[90%] w-full md:w-[90%] flex flex-col justify-center items-center gap-6 px-6 md:mx-auto border border-[#1B444B]/70">
+                <h1 className="text-2xl md:text-3xl font-bold">Together Zone</h1>
+                <div className=" flex flex-wrap justify-center gap-5 w-full md:max-w-lg p-4">
                     {features.map((f) => (
                         <button
                             key={f.id}
