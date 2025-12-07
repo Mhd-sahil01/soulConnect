325352d851540b474f6d584230a052bf2b9f60d0
Mhd-sahil01 <muhammedsahil274@gmail.com>
2025-12-07 12:45:55 +0530

    add the chat layout for both sender and receiver

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,13 +1,50 @@
 import Particles from "../components/Particles.jsx";
 
 export default function ChatPage() {
+  const leftCurveTail = {
+    content: '""',
+    position: "absolute",
+    bottom: "0px",
+    left: "-22px",
+    width: "40px",
+    height: "25px",
+    background: "radial-gradient(25px at top left, #0000 99%, #00A3FF 102%)"
+  };
+
+  const rightCurveTail = {
+    content: '""',
+    position: "absolute",
+    bottom: "0px",
+    right: "-22px",
+    width: "40px",
+    height: "25px",
+    background: "radial-gradient(25px at top right, #0000 99%, #7A5AF5 102%)"
+  };
   return (
     <>
       <div className="relative h-screen w-full bg-black overflow-hidden text-white">
-        <div className="absolute h-[80vh] w-[85vw] bg-[#0C0F13]/60 backdrop-blur-lg border border-[#1B444B]  rounded-xl z-20 inset-0 m-auto">
-          <div>
-            // edit later
+        <div className="absolute h-[80vh] w-[85vw] bg-[#0C0F13]/60 backdrop-blur-lg border border-[#1B444B]  rounded-xl z-20 inset-0 m-auto px-2">
+
+          <div className="p-4 space-y-6 w-full">
+            {/* Chat Start */}
+            <div className="flex justify-start w-full">
+              <div className="relative max-w-xs md:max-w-sm p-3 rounded-2xl bg-[#00A3FF] text-white text-sm">
+                hey joy fsv s Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aliquid omnis sequi aut, cupiditate deleniti. Hic placeat voluptas accusantium, odit repudiandae dolorem quae?
+                <span className="block text-[10px] text-white/70 mt-1">12:15pm</span>
+                <span style={leftCurveTail}></span>
+              </div>
+            </div>
+
+            {/* Chat End */}
+            <div className="flex justify-end w-full">
+              <div className="relative max-w-xs md:max-w-sm p-3 rounded-2xl bg-[#7A5AF5] text-white text-sm">
+                hello ratial how are you fsns s bskb Lorem ipsum dolor sit amet.
+                <span className="block text-[10px] text-white/70 mt-1">12:18pm</span>
+                <span style={rightCurveTail}></span>
+              </div>
+            </div>
           </div>
+
         </div>
         <Particles className="absolute inset-0 z-0"
           particleColors={['#00FFFF', '#00FFFF']}
